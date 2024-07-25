8e2c5f7393fce037790179e7b60bd3a7b9188f2a
taijitsuhitsyou <hy.nguyenduc452@gmail.com>
2024-07-25 13:11:20 +1000

    rendering content from local storage

--- src/pokemonEncounters/browserSetup.js
+++ src/pokemonEncounters/browserSetup.js
@@ -6,11 +6,58 @@ let encounterSixButton = document.getElementById("encounter-six");
 encounterOneButton.addEventListener("click", async () => {
     let result = await getOnePokemon();
     console.log(result);
+    result = [result];
+    localStorage.setItem("wildPokemon", JSON.stringify(result));
 
+    renderData();
     // getOnePokemon().then(data => console.log(data));
 })
 
 encounterSixButton.addEventListener("click", async () => {
     let result = await getMultiplePokemon();
     console.log(result);
+    localStorage.setItem("wildPokemon", JSON.stringify(result));
+
+    renderData();
 })
+
+function renderData() {
+    // Retrieve data from local storage
+    let pokemonData = localStorage.getItem("wildPokemon");
+
+    // console.log(pokemonData);
+    // console.log(typeof pokemonData);
+
+    pokemonData = JSON.parse(pokemonData) // changes string information into object data
+
+    // Reference to reuse
+    let wildPokemonContainer = document.getElementById("pokemon-encounters");
+    wildPokemonContainer.innerHTML = "";
+
+    pokemonData.forEach(pokemon => {
+        // Build HTML elements to display the data
+        let newElement = buildPokemonDisplayElement(pokemon);
+
+        // Insert the new HTML element into the current page
+        wildPokemonContainer.appendChild(newElement);
+    });
+
+}
+
+function buildPokemonDisplayElement(pokemonData) {
+    if (!pokemonData) {
+        return;
+    }
+
+    let pokemonContainer = document.createElement("div");
+
+    let pokemonHeading = document.createElement("h1");
+    pokemonHeading.innerText = pokemonData.name;
+    pokemonContainer.appendChild(pokemonHeading);
+
+    let pokemonImage = document.createElement("img");
+    pokemonImage.src = pokemonData.sprites.front_default;
+    pokemonContainer.appendChild(pokemonImage);
+
+    return pokemonContainer;
+}
